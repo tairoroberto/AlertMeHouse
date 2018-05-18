fdaa77fac18bbc412584cb5e3ad1c8979336fa5f
tairoroberto <tairoroberto@hotmail.com>
2018-05-18 11:00:44 -0300

    ajuste de topico

--- app/src/main/kotlin/br/com/alertmehouse/alertmehouse/MainActivity.kt
+++ app/src/main/kotlin/br/com/alertmehouse/alertmehouse/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import br.com.alertmehouse.alertmehouse.view.home.HomeFragment
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all")
+
         if (savedInstanceState == null) {
             replaceFragment(HomeFragment(), HOME)
         }
